e540bd125f711c906375d9eabf77f9f29573ccdd
liuxindian <liuxindian@cmcm.com>
2018-04-03 10:50:24 +0800

    + cocos start project in type script

---
+++ assets/scripts/Player.ts
@@ -0,0 +1,105 @@
+// Learn TypeScript:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class Player extends cc.Component {
+    // 主角跳跃高度
+    @property
+    jumpHeight: number = 0;
+
+    // 主角跳跃持续时间
+    @property
+    jumpDuration: number = 0;
+
+    // 最大移动速度
+    @property
+    maxMoveSpeed: number = 0;
+
+    // 加速度
+    @property
+    accel: number = 0;
+
+    private accLeft: boolean;
+    private accRight: boolean;
+    private jumpAction: cc.Action;
+    private xSpeed: number;
+
+    private setJumpAction(): cc.Action {
+        // 跳跃上升
+        let jumpUp: cc.ActionInterval = cc.moveBy(this.jumpDuration, cc.p(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        // 下落
+        let jumpDown: cc.ActionInterval = cc.moveBy(this.jumpDuration, cc.p(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        // 不断重复
+        return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+    }
+
+    private setInputControl(): void {
+        var self: Player = this;
+        // 添加键盘事件监听
+        // 有按键按下时，判断是否是我们指定的方向控制键，并设置向对应方向加速
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, function(event: cc.Event.EventCustom){
+            switch(event.keyCode) {
+                case cc.KEY.a:
+                    self.accLeft = true;
+                    break;
+                case cc.KEY.d:
+                    self.accRight = true;
+                    break;
+            }
+        });
+
+        // 松开按键时，停止向该方向的加速
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, function(event: cc.Event.EventCustom){
+            switch(event.keyCode) {
+                case cc.KEY.a:
+                    self.accLeft = false;
+                    break;
+                case cc.KEY.d:
+                    self.accRight = false;
+                    break;
+            }
+        });
+    }
+
+    // LIFE-CYCLE CALLBACKS:
+    protected onLoad(): void {
+        // 初始化跳跃动作
+        this.jumpAction = this.setJumpAction();
+        this.node.runAction(this.jumpAction);
+
+        // 加速度方向开关
+        this.accLeft = false;
+        this.accRight = false;
+        // 主角当前水平方向速度
+        this.xSpeed = 0;
+
+        // 初始化键盘输入监听
+        this.setInputControl();
+    }
+
+    protected update(dt: number): void {
+        // 根据当前加速度方向每帧更新速度
+        if (this.accLeft) {
+            this.xSpeed -= this.accel * dt;
+        } else if (this.accRight) {
+            this.xSpeed += this.accel * dt;
+        }
+        // 限制主角的速度不能超过最大值
+        if (Math.abs(this.xSpeed) > this.maxMoveSpeed) {
+            // if speed reach limit, use max speed with current direction
+            this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
+        }
+
+        // 根据当前速度更新主角的位置
+        this.node.x += this.xSpeed * dt;
+    }
+}
